72afca03034db2b784bc9140d5ca6a515f074e70
luhuadong <870179822@qq.com>
2019-12-26 18:33:08 +0800

    rt_hw_interrupt_disable & enable

--- rtthread/3.0.3/include/rthw.h
+++ rtthread/3.0.3/include/rthw.h
@@ -6,6 +6,10 @@
 rt_uint8_t *rt_hw_stack_init(void       *tentry, 
 	                           void       *parameter, 
 												     rt_uint8_t *stack_addr);
+
+
+rt_base_t rt_hw_interrupt_disable(void);
+void      rt_hw_interrupt_enable(rt_base_t level);
 														 
 /*
  * Context interfaces
